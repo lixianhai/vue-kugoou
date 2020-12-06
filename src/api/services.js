e2db3f16f48a7cf12d307cd750aa6d420ce143e7
李现海 <lixianhai@yinhechuangxiang.com>
2020-12-06 21:55:23 +0800

    ssss

--- src/api/services.js
+++ src/api/services.js
@@ -1,5 +1,5 @@
 
 // export const server_root = ''
 
-export const server_root = process.env.NODE_ENV === 'production' ? 'http://m.kugou.com' : (process.env.NODE_ENV === 'development' ? '' : '');     // 域名服务器
+export const server_root = process.env.NODE_ENV === 'production' ? '/api_sys' : (process.env.NODE_ENV === 'development' ? '' : '');     // 域名服务器
 // export const server_root = 'http://dev.erp.yinhechuangxiang.com/sys' // 域名服务器
